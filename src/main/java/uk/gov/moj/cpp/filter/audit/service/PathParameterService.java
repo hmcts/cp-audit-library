f72a0caf4bc6f003b9c0bf464335ed2f512c6304
coling01 <colin.greenwood@hmcts.net>
2025-10-20 14:59:32 +0100

    feat: audit-library initial copy from cp-access-facade
    Initial copy with trimmed down pipelines
    i.e. Drop docker and pact related actions
    
    Initially bring in code as-is ... will do some tidy ups shortly

---
+++ src/main/java/uk/gov/moj/cpp/filter/audit/service/PathParameterService.java
@@ -0,0 +1,14 @@
+package uk.gov.moj.cpp.filter.audit.service;
+
+import java.util.Map;
+
+@SuppressWarnings("PMD.ImplicitFunctionalInterface")
+public interface PathParameterService {
+    /**
+     * Extracts path parameters from the given servlet path.
+     *
+     * @param servletPath the servlet path to extract parameters from
+     * @return a map of path parameter names to their corresponding values
+     */
+    Map<String, String> getPathParameters(String servletPath);
+}
